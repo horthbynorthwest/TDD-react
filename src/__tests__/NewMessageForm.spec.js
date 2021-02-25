77121c0008d0d0c155a5d5c27c425eff1d90ea94
Georgie Horth <georgiehorth@gmail.com>
2021-02-25 12:15:36 +0000

    Added unit test which is passing, feature test still failing

--- src/__tests__/NewMessageForm.spec.js
+++ src/__tests__/NewMessageForm.spec.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import NewMessageForm from '../NewMessageForm';
 
 describe('<NewMessageForm />', () => {
     let getByTestId;
 
+    afterEach(cleanup);
+
     describe('clicking the send button', () => {
-        beforeEach(async () => {
-            ({ getByTestId } = render(<NewMessageForm />));
+        let sendHandler;
+
+        beforeEach( async() => {
+            sendHandler = jest.fn().mockName('sendHandler');
+            ({ getByTestId } = render(<NewMessageForm onSend={sendHandler} />));
 
             await userEvent.type(
                 getByTestId('messageText'),
@@ -19,6 +24,10 @@ describe('<NewMessageForm />', () => {
 
         it('clears the text field', () => {
             expect(getByTestId('messageText').value).toEqual('');
+        });
+
+        it('calls the send handler', () => {
+            expect(sendHandler).toHaveBeenCalledWith('New message');
         });
     });
 });
