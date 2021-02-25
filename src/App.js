66cd36fb9ac87437a0e377f5a3d4c7b95ccbf2c3
Georgie Horth <georgiehorth@gmail.com>
2021-02-25 11:45:49 +0000

    Added NewMessageForm component. Different error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import NewMessageForm from './NewMessageForm';
 
 const App = () => {
   return (
-    <div />
+    <div>
+      <NewMessageForm />
+    </div>
   );
 };
 
