823eea939fb7deb150b905c6066edea65909708d
Georgie Horth <georgiehorth@gmail.com>
2021-02-25 11:53:43 +0000

    Added React Testing Library test suite

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -4,9 +4,14 @@ const NewMessageForm = () => {
     return(
         <div>
             <input
-                type="text"
-                data-testid="messageText"
+              type="text"
+              data-testid="messageText"
             />
+            <button
+              data-testid="sendButton"
+            >
+                Send
+            </button>
         </div>
     );
 };
